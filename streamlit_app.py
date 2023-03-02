a45df496f4f616202d90a9deed99bf78a35fa7a9
HiteshSomani <126371849+HiteshSomani@users.noreply.github.com>
2023-03-02 00:13:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,3 @@ streamlit.dataframe(my_data_rows)
 fruit_add = streamlit.text_input('What fruit would you like to add ?','Kiwi')
 streamlit.write('Thank You for adding ', fruit_add)
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
-
